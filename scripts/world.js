2d192230df1b6076db299e73351b80a07a396d50
SuperVK <victor.klomp@gmail.com>
2018-10-20 17:52:54 +0200

    fixed some bugs and moved the broadcast method to the socket handler

--- scripts/world.js
+++ scripts/world.js
@@ -4,35 +4,28 @@ const SocketHandler = require('./SocketHandler.js')
 class World {
     constructor(id) {
         this.id = id
-        this.players = []
+        this.players = {}
+        this.buildings = {}
         this.socketHandler = new SocketHandler(this)
     }
     tick() {
-        tick()
+        tick(this)
     }
     addPlayer(socket) {
         //joins the room
         socket.join(this.id)
 
         this.socketHandler.addSocket(socket)
-        this.players.push({
+        this.players[socket.id] = {
             id: socket.id,
             pos: {
                 x: 0,
                 y: 0
             }
-        })
+        }
 
     }
-    /**
-     * 
-     * @param {String} channel the channel of the socket (eg: chatMessage, move etc)
-     * @param {Object} data The data that needs to be send over
-     * @param {Object} socket The socket which will broadcast the message
-     */
-    broadcast(channel, data, socket) {
-        
-    }
+    
 
 }
 module.exports = World
