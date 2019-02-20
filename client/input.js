033a6b8641025858c3646c59b07bc22f09405803
Marto_0 <martvanenck1@gmail.com>
2019-02-20 15:26:13 +0100

    fixed infinite tap walking glitch

--- client/input.js
+++ client/input.js
@@ -1,19 +1,29 @@
-
+let latestKeyDown = Date.now()
 function keyListener() {
   $(document).keyup(function(event) {
+    if(!player.keys[event.keyCode]) return
     player.keys[event.keyCode] = false
 
-    // WASD moving
-    if(!player.typing) {
+    // wait 10ms. Until keydown has been send
+    let timeout = (latestKeyDown - Date.now())+100
+    console.log(timeout)
+    if(timeout < 0) timeout = 1
+    setTimeout(() => {
+      // WASD moving
+      if(!player.typing) {
+
         let kc = event.keyCode
         if((kc == 87 || kc == 38) && player.moving.north) socket.emit('PLAYER_DATA', {type: 'movement', world: player.world, direction: 'north', isDown: false})
         if((kc == 68 || kc == 39) && player.moving.east) socket.emit('PLAYER_DATA', {type: 'movement', world: player.world, direction: 'east', isDown: false})
         if((kc == 83 || kc == 40) && player.moving.south) socket.emit('PLAYER_DATA', {type: 'movement', world: player.world, direction: 'south', isDown: false})
         if((kc == 65 || kc == 37) && player.moving.west) socket.emit('PLAYER_DATA', {type: 'movement', world: player.world, direction: 'west', isDown: false})
-    }
+      }
+    }, timeout)
   })
   $(document).keydown(function(event) {
+    if(player.keys[event.keyCode]) return
     player.keys[event.keyCode] = true
+    latestKeyDown = Date.now()
 
     // WASD moving
     if(!player.typing) {
